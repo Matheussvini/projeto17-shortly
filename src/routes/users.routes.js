909e105fd17197b9b4d1aac0cf6c20dc70a4fa44
matheussvini <matheussvini@outlook.com>
2022-12-23 04:15:15 -0300

    feat: auth validation

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { createUser, findById, login } from "../controllers/users.controllers.js";
+import authValidation from "../middlewares/authValidation.js";
 import { validShemaSignIn, validShemaSignUp } from "../middlewares/users.middlewares.js";
 
 const router = Router();
 
 router.post('/signup', validShemaSignUp, createUser);
 router.post('/signin', validShemaSignIn, login);
-router.get('/users/me', findById);
+router.get('/users/me', authValidation , findById);
 
 export default router;
