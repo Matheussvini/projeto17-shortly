16456fa8334107867be194895eb8609667ebc64e
matheussvini <matheussvini@outlook.com>
2022-12-23 16:30:27 -0300

    scripts tests sql

--- scripts-teste.sql
+++ scripts-teste.sql
@@ -1,10 +1,25 @@
 CREATE DATABASE shortlydb
 
+\c shortlydb
 
+INSERT INTO urls (url, "shortUrl", user_id)
+VALUES ('http:/globo.com', 'hhhhh', 8);
 
- 
+INSERT INTO users (name, email, password)
+VALUES ('caio', 'caio2@gmail.com', '123456');
 
+SELECT * FROM urls;
+SELECT * FROM users;
 
+DROP TABLE urls;
+DROP TABLE users;
+
+CREATE TABLE users(
+	"id" SERIAL NOT NULL PRIMARY KEY,
+	"name" varchar(60) NOT NULL,
+	"email" TEXT NOT NULL UNIQUE,
+	"password" TEXT NOT NULL
+);
 
 CREATE TABLE urls(
 	"id" SERIAL NOT NULL PRIMARY KEY,
@@ -15,43 +30,16 @@ CREATE TABLE urls(
 	"createdAt" TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW()
 );
 
-INSERT INTO urls (url, "shortUrl", user_id)
-VALUES ('http:/globo.com', 'hhhhh', 8);
-
-INSERT INTO users (name, email, password)
-VALUES ('caio', 'caio2@gmail.com', '1234');
-
-SELECT * FROM urls;
-SELECT * FROM users;
-
-DROP TABLE urls CASCADE;
-
-CREATE TABLE urls(
-	"id" SERIAL NOT NULL PRIMARY KEY,
-	"url" TEXT NOT NULL,
-	"shortUrl" TEXT UNIQUE,
-	"user_id" integer NOT NULL REFERENCES users(id),
-	"visits" integer NOT NULL DEFAULT 0
-);
-
-CREATE TABLE users(
-	"id" SERIAL NOT NULL PRIMARY KEY,
-	"name" varchar(60) NOT NULL,
-	"email" TEXT NOT NULL UNIQUE,
-	"password" TEXT NOT NULL
-);
-
 
 SELECT
-                users.id,
-                users.name,
-                SUM(urls.visits) AS "visitCount"
-
-            FROM users
-            JOIN urls
-            ON users.id = urls.user_id
-            WHERE urls.user_id = 3
-			GROUP BY users.id;
+	users.id,
+	users.name,
+	SUM(urls.visits) AS "visitCount"
+FROM users
+JOIN urls
+ON users.id = urls.user_id
+WHERE urls.user_id = 3
+GROUP BY users.id;
 			
 			
 SELECT
@@ -64,7 +52,7 @@ JOIN users
 ON urls.user_id = users.id
 WHERE urls.user_id = 1
 GROUP BY urls.id
-ORDER BY urls.id ASC
+ORDER BY urls.id ASC;
 			
 		
 SELECT * FROM users WHERE email = 'aaa@gmail.com';
@@ -73,3 +61,15 @@ SELECT * FROM users WHERE email = 'aaa@gmail.com';
 UPDATE urls
 SET visits = visits + 1
 WHERE id = 2;
+
+SELECT 
+users.id,
+users.name,
+COUNT(urls.id) AS "linksCount",
+COALESCE(SUM(urls.visits), 0) AS "visitCount"
+FROM users
+LEFT JOIN urls
+ON urls.user_id = users.id
+GROUP BY users.id
+ORDER BY "visitCount" DESC
+LIMIT 10;
